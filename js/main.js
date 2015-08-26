fec4d57a4a1275e2c9ee2eba3961bb61ffdf9978
maksimgm <maksimmamrikov@gmail.com>
2015-08-25 22:59:11 -0700

    add notes. add function

---
+++ js/main.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+// var numOne;
+// var numTwo;
+// var operator = $("#calc");
+
+// var screen;
+
+
+
+var screen = $("#screen");  
+  if(screen===null){
+    parseInt((this).text(0));
+  }
+
+
+
+
+$(".buttons").click(function(){
+  $(this).val() = screen.text() ;
+
+});
+
+
+
+
+
+
+
+
+
+// if ("#screen"===undefined) {
+//     $("#screen").text= 0;
+//   }else{
+//     $("span").click(function(){
+//     var selection = $(this).text();
+//     $("#screen").text(selection);
+//   });
+// }
+
+
+});
+
+
+
+// display more than one number... with operators.
+
+// try using short helper functions, then putting everything together
+// modulerize!!! very important.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
